aaaf5070408d2203990d259b97136cd73ec9faa6
Uzun-Tatiana <76948784+Uzun-Tatiana@users.noreply.github.com>
2021-01-04 22:22:13 +0300

    Add files via upload

---
+++ Lab_6_informatika.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+#include <stdio.h>
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+
+
+int main(void) {
+    setlocale(0, ""); // Включаем кириллицу
+    char str[255], temp;
+    int n, i, j;
+    cout << "Введите строку" << endl;
+    fgets(str, 255, stdin);
+    fflush(stdin); // очищаем поток ввода
+
+    cout << "Введенная строка" << endl;
+    puts(str);
+
+    cout << "Элемeнты строки в алфавитном порядке" << endl;
+
+    n = strlen(str);
+
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
+            if (str[i] > str[j])
+            {
+                temp = str[i];
+                str[i] = str[j];
+                str[j] = temp;
+            }
+        }
+    }
+    printf("%s", str);
+
+    return 0;
+}
